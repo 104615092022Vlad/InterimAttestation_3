014012e82843a912219fc675e6f315db97d503e6
Vlad <ystimenko_vlad@mail.ru>
2023-07-06 23:00:59 +0300

    Revert and bound category index to 5

--- src/test/java/ru/aptekaeconom/test/AptekaTest.java
+++ src/test/java/ru/aptekaeconom/test/AptekaTest.java
@@ -31,7 +31,7 @@ public class AptekaTest {
     @RepeatedTest(10)
     public void selectAnySubcategory() {
         Random r = new Random();
-        int n = 5;
+        int n = r.nextInt(0,5);
         String categoryName;
         String subcategoryName;
         PageTop pageTop = new PageTop();
